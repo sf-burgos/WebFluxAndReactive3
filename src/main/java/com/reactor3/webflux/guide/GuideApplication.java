e2100bd57fcc982db564d67c9b8d869f22e67ea8
PERFICIENT\brayan.burgosd <brayanburgos1437@gmail.com>
2024-01-04 12:47:37 -0500

    Guide2, Async method with DelayElements

--- src/main/java/com/reactor3/webflux/guide/GuideApplication.java
+++ src/main/java/com/reactor3/webflux/guide/GuideApplication.java
@@ -1,9 +1,12 @@
 package com.reactor3.webflux.guide;
 
+import com.reactor3.webflux.guide.configuration.ManualConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Import;
 
 @SpringBootApplication
+@Import(ManualConfiguration.class)
 public class GuideApplication {
 
 	public static void main(String[] args) {
